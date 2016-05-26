75d4d02ca29313de40511b99953c05b53182c99e
木木 <2227820226@QQ.com>
2016-05-26 23:09:52 +0800

    修改传入参数是特殊格式

--- locate.js
+++ locate.js
@@ -5,22 +5,55 @@
 function onRequest(request, response, modules) {
     //获得oData的对象
     var db = modules.oData;
-    // console.log(request.body.result);
+    // console.log(request.body);
+    var num = request.body.num;
+    var bssids = "\"BSSIDs\":[";
+    var rssis = "\"RSSIs\":[";
+    for (var i = 0; i < num; i++) {
+        switch (i) {
+            case 0:
+                bssids += "\"" + request.body.B1 + "\",";
+                rssis += "\"" + request.body.R1 + "\",";
+                break;
+            case 1:
+                bssids += "\"" + request.body.B2 + "\",";
+                rssis += "\"" + request.body.R1 + "\",";
+                break;
+            case 2:
+                bssids += "\"" + request.body.B3 + "\",";
+                rssis += "\"" + request.body.R3 + "\",";
+                break;
+            default:
+                break;
+        }
+
+    }
+    bssids = bssids.substr(0, bssids.length - 1);
+    rssis = rssis.substr(0, rssis.length - 1);
+    bssids += "]";
+    rssis += "]";
+    result2 = bssids + "," + rssis;
+    result2 = "{" + result2 + "}";
+    result2 = JSON.parse(result2);
+
     db.find({
         "table": "WifiInfo"
     }, function (err, data) {
         //将data对象转化为json对象
         var resultObject = JSON.parse(data);
         //获取每行的值
+        // console.log(resultObject);
         var minDistance = 100000;
         var location;
 
         function distanceOf(result, result2) {
             var distance = 0;
+            var count = 0;
             // console.log("result=\n" + result);
             // console.log(result.BSSIDs["1"]);
             // console.log("result2=\n" + result2);
             for (var p1 in result.BSSIDs) {
+                count++;
                 var BSSID = result.BSSIDs[p1];
                 // console.log("bssid" + BSSID);
                 var RSSI = result.RSSIs[p1];
@@ -37,6 +70,7 @@ function onRequest(request, response, modules) {
                     distance += Math.pow(RSSI, 2);
                 }
             }
+            console.log(count);
             for (var p1 in result2.BSSIDs) {
                 var BSSID = result2.BSSIDs[p1];
                 // console.log("bssid" + BSSID);
@@ -71,8 +105,9 @@ function onRequest(request, response, modules) {
                  */
                 var result = resultArr[oneLine];
                 // console.log(result);
-                var distance = distanceOf(result, request.body.result);
-                // console.log(distance + "\n");
+                // console.log(request.body);
+                var distance = distanceOf(result, result2);
+                console.log(distance + "\n");
                 if (distance < minDistance) {
                     minDistance = distance;
                     location = result;
@@ -81,7 +116,7 @@ function onRequest(request, response, modules) {
             }
         }
         // response.end(location);
-        console.log(location.BuildingName + location.RoomName + "(x="+location.x+",y="+location.y+")");
+        console.log(location.BuildingName + location.RoomName + "(x=" + location.x + ",y=" + location.y + ")");
     });
 }
 exports.locate = onRequest;
